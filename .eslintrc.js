21220b5fa0c446b0cfaa59297fb6105c06aea254
huantingwei <ting02940@gmail.com>
2020-08-21 00:48:45 +0800

    feat: add eslint formatting

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    parserOptions: {
+        parser: 'babel-eslint',
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    extends: ['plugin:react/recommended', 'plugin:prettier/recommended'],
+    plugins: ['react-hooks'],
+    // add your custom rules here
+    rules: {
+        'react/prop-types': 1,
+        'react-hooks/rules-of-hooks': 'error', // 檢查 Hook 的規則
+        'react-hooks/exhaustive-deps': 'warn', // 檢查 effect 的相依性
+    },
+}
